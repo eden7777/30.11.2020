6bdf5db7272e1c57ed1bd5281911982a9bbee24a
qa191120 <74743291+qa191120@users.noreply.github.com>
2020-11-30 18:31:54 +0200

    Create var_modify.java

---
+++ var_modify.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        boolean is_veg = true;
+        System.out.println(is_veg);
+        int x = -5;
+        boolean is_x_positive = x > 0;
+        char letter = 'A';
+
+        // y = 7
+        // *7
+        // + 12
+        // - 20
+        int y = 9;
+        int z = y * 7;
+        // 1. calculate a value using variable
+        // 2. put the results into the same variable
+        y = y * 7; // 9 * 7 = 63 ==> y
+        int x1 = 1;
+        // put into x1 <== x1 plus 1
+        x1 = x1 + 1;
+        System.out.println(y);
+    }
+}
