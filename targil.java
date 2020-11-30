33009137c27eaa9560d4c610beecf7a28fa0c2b9
qa191120 <74743291+qa191120@users.noreply.github.com>
2020-11-30 20:18:13 +0200

    Create targil.java

---
+++ targil.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+       // debugging
+        // break point
+        int a = 15;
+        int b = 200;
+        int sum = a + b;
+        System.out.println(sum);
+        
+        // define fname = get the name from keyboard (Scanner)
+        // define lname = get the name from keyboard (Scanner)
+        // print (fname + " " + lname)
+        // run in debugger 
+        // after scanner of lname, change the fname to:"Omer"
+        // change lname to "Adam" --> do it with "set value" debugger
+        // now see it the print actually printed Omer Adam
+        
+
+    }
+}
